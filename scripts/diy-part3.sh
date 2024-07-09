5f2ec598f07fa7b4991c707c8b618294868c3257
HoioH <67760304+KK2018gh@users.noreply.github.com>
2024-07-09 12:25:22 +0800

    Add files via upload

---
+++ scripts/diy-part3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (After Install feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# 替换golang版本为1.22
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# ttyd免登陆
+sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# design修改proxy链接
+sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
